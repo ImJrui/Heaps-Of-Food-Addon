308a74ae9c10237ea0f8eb09520c4b0fa84a6e9a
ImJrui <huangjianrui1997@163.com>
2026-08-21 10:11:28 +0800

    Initial commit

---
+++ modinfo.lua
@@ -0,0 +1,46 @@
+local lang = locale
+local function translate(String)  -- use this fn can be automatically translated according to the language in the table
+	String.zhr = String.zh
+	String.zht = String.zht or String.zh
+	return String[lang] or String.en
+end
+
+--The name of the mod displayed in the 'mods' screen.
+name = translate({en = "Heap Of Foods Addon", zh = "HOP补丁"})
+
+--A description of the mod.
+description = "version v1.0.0"
+
+--Who wrote this awesome mod?
+author = "TUTU"
+
+--A version number so you can ask people if they are running an old version of your mod.
+version = "v1.0.0"
+
+--This lets other players know if your mod is out of date. This typically needs to be updated every time there's a new game update.
+api_version = 10
+
+dst_compatible = true
+
+--This lets clients know if they need to get the mod from the Steam Workshop to join the game
+all_clients_require_mod = true
+
+--This determines whether it causes a server to be marked as modded (and shows in the mod list)
+client_only_mod = false
+
+--This lets people search for servers with this mod by these tags
+server_filter_tags = {}
+
+
+priority = -20  --模组优先级0-10 mod 加载的顺序   0最后载入  覆盖大值
+
+configuration_options={ --模组变量配置
+
+}
+
+mod_dependencies = {
+
+}
+
+icon_atlas = "ModiconHOF.xml"
+icon = "ModiconHOF.tex"
---
+++ modmain.lua
@@ -0,0 +1,14 @@
+local modimport = modimport
+
+modimport("main/toolutil")
+
+modimport("scripts/components/new_forgerepair")
+modimport("scripts/components/new_sisturnregistry")
+
+modimport("scripts/rebalance/ghostflowerhat")
+modimport("scripts/rebalance/abigail")
+modimport("scripts/rebalance/ghostly_elixirs")
+modimport("scripts/rebalance/grave")
+modimport("scripts/rebalance/moondial")
+modimport("scripts/rebalance/recipes")
+modimport("scripts/rebalance/skilltree_wendy")
